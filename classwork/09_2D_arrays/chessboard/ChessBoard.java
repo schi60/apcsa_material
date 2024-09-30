960e72453739f06c7784e2ef4ec537f87de507fd
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-09-29 22:21:08 -0400

    Create ChessBoard.java

---
+++ classwork/09_2D_arrays/chessboard/ChessBoard.java
@@ -0,0 +1,18 @@
+public class ChessBoard
+{
+    public static void main(String[] args)
+    {
+        //Create an 8x8 2D String array called chess.
+        
+        
+        //Use this method to print the chess board onto the console
+        print(chess);
+        
+
+    }
+    
+    public static void print(String[][] array)
+    {
+
+    }
+}
