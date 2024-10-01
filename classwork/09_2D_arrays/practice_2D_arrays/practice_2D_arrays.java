964abf46beb2fb83c2baccc99d0baadec1694a33
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-10-01 13:23:13 -0400

    Create practice_2D_arrays.java

---
+++ classwork/09_2D_arrays/practice_2D_arrays/practice_2D_arrays.java
@@ -0,0 +1,62 @@
+public class TwoDArrayPractice
+{
+
+  public static void main(String[] args)
+    {
+      // Create test cases here.
+      
+      // Print the status of your 2D array before and after
+      // invoking a method. You will need to add a print() method
+      // to avoid duplicated code in your main.
+    }
+
+  
+    /*
+     * Swaps all values in the specified 2 rows of array.
+    */
+  public static void rowSwap(int[][] array, int rowAIndex, int rowBIndex)
+    {
+        // Yor code here
+    }
+
+  
+    /**
+     * Swaps all values in the specified 2 columns of array.
+     */
+    public static void colSwap(int[][] array, int colAIndex, int colBIndex)
+    {
+        // Your code her
+    }
+
+  
+    /*
+     * Returns an array with the specified number of rows and columns
+     * containing the characters from str in row-major order. If str.length()
+     * is greater than rows * cols, extra characters are ignored. If
+     * str.length() is less than rows * cols, the remaining elements in the
+     * returned array contain null.
+
+     Examples:
+
+      String[][] result = fillRows("Happy Halloween", 3, 4);
+      
+      result (after method call):
+      "H"     "a"     "p"     "p"
+      "y"     " "     "H"     "a"
+      "l"     "l"     "o"     "w"
+      
+      
+      String[][] result = fillRows("hello", 3, 4);
+      
+      result (after method call):
+      "h"     "e"     "l"     "l"
+      "o"     null    null    null
+      null    null    null    null
+
+     */
+    public static String[][] fillRows(String str, int rows, int cols)
+    {
+        return null; // replace this with your 2D array
+    }
+  
+}    
