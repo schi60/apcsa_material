ef742359f050e65451cf55564397233823063c1b
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-10-08 08:01:42 -0400

    Create CopyArray.java

---
+++ classwork/10_copy_array/CopyArray.java
@@ -0,0 +1,6 @@
+// Define a class
+// Write the main method
+// Implement the copy method. It should create a copy of nums. If you change the new array, nums should not change.
+// Write multiple test cases.
+public static int[][] copy(int[][] nums){
+}
