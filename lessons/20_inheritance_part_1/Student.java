e369ec88a64cc4d6f93aef1d0b18e8a71508de11
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-11-12 13:39:36 -0500

    Create Student.java

---
+++ lessons/20_inheritance_part_1/Student.java
@@ -0,0 +1,17 @@
+public class Student extends Person {
+    private int grade;
+
+    public Student(String fName, String lName, String addr, int gr) {
+        super(fName, lName, addr);
+        grade = gr;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int newInput) {
+        grade = newInput;
+    }
+
+}
