5b03b3ce177674ddd1ca3e80b8737517c5a1e1ef
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2024-11-12 13:39:08 -0500

    Create Person.java

---
+++ lessons/20_inheritance_part_1/Person.java
@@ -0,0 +1,38 @@
+public class Person {
+    private String firstName, lastName, address;
+
+    public Person(String fName, String lName, String addr) {
+        firstName = fName;
+        lastName = lName;
+        address = addr;
+    }
+
+    public Person() {
+        firstName = "default";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setFirstName(String newInput) {
+        firstName = newInput;
+    }
+
+    public void setLastName(String newInput) {
+        lastName = newInput;
+    }
+
+    public void setAddress(String newInput) {
+        address = newInput;
+    }
+
+}
